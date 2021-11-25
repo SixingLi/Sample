a8b7e09241ccc6147958fcc52fa2088e2fad9d1b
qinggele <qinggele@51hitech.com>
2021-11-25 14:00:28 +0800

    update API Samples

---
+++ Samples/ROS_3.0/build_release/CMakeFiles/trans_node_ros.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../run/trans_node_ros"
+  "../run/trans_node_ros.pdb"
+  "CMakeFiles/trans_node_ros.dir/src/Config.cpp.o"
+  "CMakeFiles/trans_node_ros.dir/src/trans_node_ros.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trans_node_ros.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Samples/ROS_3.0/build_release/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/oliver/Documents/Git/Master/Cybertron/Modules/SimOneAPI/Samples/ROS_3.0/build_release/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH='/opt/ros/melodic/lib:/home/oliver/rti_connext_dds-6.0.1/lib/x64Linux4gcc7.3.0:/usr/local/cuda/lib64:/home/oliver/rti_connext_dds-6.0.1/lib/x64Linux4gcc7.3.0:/usr/local/cuda/lib64'
+export PATH='/opt/ros/melodic/bin:/home/oliver/rti_connext_dds-6.0.1/bin:/usr/local/cuda/bin:/home/oliver/rti_connext_dds-6.0.1/bin:/usr/local/cuda/bin:/home/oliver/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/home/oliver/ProgramFiles/go1.16.5.linux-amd64/go/bin:/home/oliver/ProgramFiles/go1.16.5.linux-amd64/go/bin'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/oliver/Documents/Git/Master/Cybertron/Modules/SimOneAPI/Samples/ROS_3.0/build_release/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/home/oliver/Documents/Git/Master/Cybertron/Modules/SimOneAPI/Samples/ROS_3.0:$ROS_PACKAGE_PATH"
