3e36c7b5751e93dbfb0d99f7b4fbbe3ae7d8ae97
gaofeng <gaofeng@51hitech.com>
2021-12-09 22:56:38 +0800

    update simone test api

--- InternalTest/TestAPI/main.cpp
+++ InternalTest/TestAPI/main.cpp
@@ -10,25 +10,48 @@
 
 void Test_V2X(const char * MainVehicleID, bool IsCallBackMode);
 void Test_GPS(const char * MainVehicleID, bool IsCallBackMode);
-void Test_UltrasonicRadar(const char * MainVehicleID,bool IsCallBackMode);
+void Test_UltrasonicRadar(const char * MainVehicleID);
+void Test_UltrasonicRadars(const char * MainVehicleID, bool IsCallBackMode);
 void Test_SensorLaneInfo(const char * MainVehicleID,bool IsCallBackMode);
 void Test_SensorSensorDetection(const char * MainVehicleID, bool isCallBackMode);
+void Test_RadarDetection(const char * MainVehicleID, bool isCallBackMode);
 using namespace std;
 int main(int argc, char* argv[])
 {
 	bool isJoinTimeLoop = false;
-	string MainVehicleId = "1";
+	string MainVehicleId = "0";
 	SimOneAPI::InitSimOneAPI(MainVehicleId.c_str(), isJoinTimeLoop);
 	
 	//Test_V2X(false);
-	//Test_UltrasonicRadar(false);
-	//Test_SensorLaneInfo(false);
-	//Test_GPS("0",false);
-	Test_SensorSensorDetection(MainVehicleId.c_str(),true);
+	//Test_UltrasonicRadars(MainVehicleId.c_str(),false);
+	//Test_UltrasonicRadar(MainVehicleId.c_str());
+	//Test_SensorLaneInfo(MainVehicleId.c_str(),true);
+	//Test_GPS(MainVehicleId.c_str(),true);
+	Test_SensorSensorDetection(MainVehicleId.c_str(),false);
+	//Test_RadarDetection(MainVehicleId.c_str(), false);
 	system("pause");
 	return 0;
 }
 
+void Test_RadarDetection(const char * MainVehicleID, bool IsCallBackMode) {
+	if (IsCallBackMode) {
+		auto function = [](const char* mainVehicleId, const char* sensorId, SimOne_Data_RadarDetection *pDetections) {
+			std::cout <<"mainVehicleId:"<< mainVehicleId<<", pDetections->frame:"<<pDetections->frame << ", pDetections->detectNum:"<<pDetections->detectNum <<endl;//The Lane's leftLane ID 
+			for (auto i = 0; i < pDetections->detectNum;i++) {
+				std::cout <<" test.rcsdb:" << pDetections->detections[i].rcsdb << "," <<" test.rcsdb:"<< pDetections->detections[i].probability << endl;//The Lane's leftLane ID 
+			}
+		};
+		SimOneAPI::SetRadarDetectionsUpdateCB(function);
+	}
+	else {
+		std::unique_ptr<SimOne_Data_RadarDetection> pDetections = std::make_unique<SimOne_Data_RadarDetection>();
+		while (true) {
+			SimOneAPI::GetRadarDetections(MainVehicleID, "objectBasedRadar1", pDetections.get());
+			std::cout <<"mainVehicleId:"<< MainVehicleID <<", pDetections->frame:"<<pDetections->frame << ", pDetections->detectNum:"<<pDetections->detectNum <<endl;//The Lane's leftLane ID 
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
+		}
+	}
+}
 void Test_SensorSensorDetection(const char * MainVehicleID, bool isCallBackMode) {
 	if (isCallBackMode) {
 		auto function = [](const char* MainVehicleID, const char* sensorId, SimOne_Data_SensorDetections *pGroundtruth) {
@@ -39,8 +62,10 @@ void Test_SensorSensorDetection(const char * MainVehicleID, bool isCallBackMode)
 	}
 	else {
 		std::unique_ptr<SimOne_Data_SensorDetections> pGroundtruth = std::make_unique<SimOne_Data_SensorDetections>();
-		while (SimOneAPI::GetSensorDetections(MainVehicleID, "objectBasedCamera1", pGroundtruth.get())) {
+		while (true) {
+			SimOneAPI::GetSensorDetections(MainVehicleID, "sensorFusion1", pGroundtruth.get());
 			std::cout << pGroundtruth->frame << "," << pGroundtruth->objectSize << endl;//The Lane's leftLane ID 
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	}
 }
@@ -54,33 +79,49 @@ void Test_SensorLaneInfo(const char * MainVehicleID, bool IsCallBackMode) {
 	}
 	else {
 		std::unique_ptr<SimOne_Data_LaneInfo> pDetections = std::make_unique<SimOne_Data_LaneInfo>();
-		while (SimOneAPI::GetSensorLaneInfo(MainVehicleID, "objectBasedCamera1", pDetections.get())) {
-			std::cout << pDetections->frame <<","<< pDetections->laneType<<","<< pDetections->laneLeftID<<","<< pDetections->laneRightID<<"," << pDetections->laneRightID<<endl;//The Lane's leftLane ID  
+		while (true) {
+			SimOneAPI::GetSensorLaneInfo(MainVehicleID, "sensorFusion1", pDetections.get());
+			std::cout << pDetections->frame <<","<< pDetections->laneType<<","<< pDetections->laneLeftID<<","<< pDetections->laneLeftID <<"," << pDetections->laneRightID<<endl;//The Lane's leftLane ID  
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	}
 }
 
-void Test_UltrasonicRadar(const char * MainVehicleID, bool IsCallBackMode){
+void Test_UltrasonicRadars(const char * MainVehicleID, bool IsCallBackMode){
 	if (IsCallBackMode) {
 		auto function = [](const char * mainVehicleId, SimOne_Data_UltrasonicRadars *pUltrasonics) {
+			if(pUltrasonics->ultrasonicRadars[0].obstacleNum>0)
 				std::cout << pUltrasonics->frame << "," << pUltrasonics->ultrasonicRadarNum << "," << pUltrasonics->timestamp << "," << pUltrasonics->ultrasonicRadars[0].obstacleDetections[0].obstacleRanges << "," << pUltrasonics->ultrasonicRadars[0].obstacleDetections[0].x << "," << pUltrasonics->ultrasonicRadars[0].obstacleDetections[0].y << endl;
 		};
 		SimOneAPI::SetUltrasonicRadarsCB(function);
 	}
 	else {
-		std::unique_ptr<SimOne_Data_UltrasonicRadar> pDetections = std::make_unique<SimOne_Data_UltrasonicRadar>();
-		while (SimOneAPI::GetUltrasonicRadar(MainVehicleID, "ultrasonic1", pDetections.get())) {
-			for (int i = 0; i < pDetections->obstacleNum; i++) {
-				std::cout << pDetections->frame << "," << pDetections->obstacleNum << "," << pDetections->obstacleDetections[i].obstacleRanges << "," << pDetections->obstacleDetections[i].x << "," << pDetections->obstacleDetections[i].y << endl;
+		std::unique_ptr<SimOne_Data_UltrasonicRadars> pDetections = std::make_unique<SimOne_Data_UltrasonicRadars>();
+		while (true) {
+			SimOneAPI::GetUltrasonicRadars(MainVehicleID, pDetections.get());
+			for (int i = 0; i < pDetections->ultrasonicRadarNum; i++) {
+				std::cout << pDetections->frame << ", " << pDetections->ultrasonicRadars[i].obstacleNum << ", " << pDetections->ultrasonicRadars[i].obstacleDetections[i].obstacleRanges << ", " << pDetections-> ultrasonicRadars[i].obstacleDetections[i].x << ", " << pDetections->ultrasonicRadars[i].obstacleDetections[i].y << endl;
 			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
+	}
+}
+
+void Test_UltrasonicRadar(const char * MainVehicleID) {
+	std::unique_ptr<SimOne_Data_UltrasonicRadar> pDetections = std::make_unique<SimOne_Data_UltrasonicRadar>();
+	while (true) {
+		SimOneAPI::GetUltrasonicRadar(MainVehicleID, "ultrasonic1", pDetections.get());
+		for (int i = 0; i < pDetections->obstacleNum; i++) {
+			std::cout << pDetections->frame << ", " << pDetections->obstacleDetections[i].x << ", " << pDetections->obstacleDetections[i].obstacleRanges << ", " << pDetections->obstacleDetections[i].x << ", " << pDetections->obstacleDetections[i].y << endl;
+		}
+		std::this_thread::sleep_for(std::chrono::milliseconds(10));
 	}
 }
 
 void Test_GPS(const char * MainVehicleID, bool IsCallBackMode) {
 	if (IsCallBackMode) {
 		auto function = []( const char* mainVehicleId, SimOne_Data_Gps *pGps){
-				std::cout<<"pGps->posX:"<<pGps->posX<<",pGps->posY"<<pGps->posY<<endl;	
+				std::cout<<"pGps->frame:"<< pGps->frame<<", pGps->posX:"<<pGps->posX<<",pGps->posY"<<pGps->posY<<endl;
 		};
 		 SimOneAPI::SetGpsUpdateCB(function);
 	}
@@ -94,6 +135,7 @@ void Test_GPS(const char * MainVehicleID, bool IsCallBackMode) {
 
 				std::cout<<"Get GPS Fail"<<endl;
 			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	}
 }
@@ -116,6 +158,7 @@ void Test_V2X(const char * MainVehicleID, bool IsCallBackMode) {
 			else {
 				std::cout << "########### GetV2XInfo Fail" << std::endl;
 			}
+			std::this_thread::sleep_for(std::chrono::milliseconds(10));
 		}
 	}
 }
