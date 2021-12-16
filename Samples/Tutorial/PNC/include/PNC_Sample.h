2156a77237c479646496b569a0f92eef2a08f916
qinggele <qinggele@51hitech.com>
2021-12-16 19:32:55 +0800

    update pnc sample

--- Samples/Tutorial/PNC/include/PNC_Sample.h
+++ Samples/Tutorial/PNC/include/PNC_Sample.h
@@ -28,20 +28,25 @@ public:
 	void set_pose_ctl();
 	void set_drive_ctl();
 	void set_drive_trajectory();
-	void get_sensor_detection();
-	static void set_v2x_info(const char* mainVehicleId, const char* sensorId, SimOne_Data_V2XNFS *pDetections) ;
+	static void get_gps(const char* mainVehicleId, SimOne_Data_Gps *pGps);
+  	static void get_sensor_detection(const char *mainVehicleId, const char *sensorId, SimOne_Data_SensorDetections *pGroundtruth);
+	static void get_sensor_laneInfo(const char* mainVehicleId, const char* sensorId, SimOne_Data_LaneInfo *pLaneInfo);
 	static void set_scenario_event(const char *mainVehicleId, const char *event, const char *data);
 
 	void pub();
 
 protected:
+	static SimOne_Data_Gps m_gps;
+	static std::atomic<bool> m_flip;
+
 	Logging::Logger log_simone_ini;
 	Logging::Logger log_set_pose_ctl;
 	Logging::Logger log_set_drive_ctl;
 	Logging::Logger log_set_drive_trajectory;
-	Logging::Logger log_get_sensor_detection;
+	static Logging::Logger log_get_gps;
+	static Logging::Logger log_get_sensor_detection;
+	static Logging::Logger log_get_sensor_laneinfo;
 	static Logging::Logger log_scenario_event;
-	static Logging::Logger log_v2x_info;
 };
 
 #endif
