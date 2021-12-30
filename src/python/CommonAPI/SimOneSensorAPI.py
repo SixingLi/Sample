3303b91f4422b4531c65f0894425e4f77234a17f
qinggele <qinggele@51hitech.com>
2021-12-30 22:08:57 +0800

    add python comments

--- src/python/CommonAPI/SimOneSensorAPI.py
+++ src/python/CommonAPI/SimOneSensorAPI.py
@@ -45,91 +45,373 @@ simoneapi_ultrasonics_cb_func = UltrasonicsCbFuncType(_simoneapi_ultrasonics_cb)
 simoneapi_radardetection_cb_func = RadarDetectionCbFuncType(_simoneapi_radardetection_cb)
 simoneapi_sensor_detections_cb_func = SensorDetectionsCbFuncType(_simoneapi_sensor_detections_cb)
 
-
 def SoGetGps(mainVehicleId, gpsData):
+	"""获取主车GPS信息.
+
+	Get main vehicle GPS
+
+	Parameters
+	----------
+	dmainVehicleI : 
+		Id of the main vehicle
+	gpsData : 
+		GPS data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetGps.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	return SimoneAPI.GetGps(_mainVehicleId, pointer(gpsData))
 
 def SoApiSetGpsUpdateCB(cb):
+	"""主车GPS更新回调.
+
+	Register the callback func applying for GPS info
+
+	Parameters
+	----------
+	cb : 
+		GPS data update callback function
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	global SIMONEAPI_GPS_CB
 	SimoneAPI.SetGpsUpdateCB(simoneapi_gps_cb_func)
 	SIMONEAPI_GPS_CB = cb
 
 def SoGetGroundTruth(mainVehicleId, obstacleData):
+	"""得到仿真场景中的物体的真值.
+
+	Get Ground Truth Data of Objects(Obstacles) from simulation scene
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	obstacleData : 
+		Obstacle data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetGroundTruth.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	ret = SimoneAPI.GetGroundTruth(_mainVehicleId, pointer(obstacleData))
 	return ret
 
 def SoApiSetGroundTruthUpdateCB(cb):
+	"""得到仿真场景中的物体的真值的更新回调.
+
+	Register the callback func applying for obstacle info
+
+	Parameters
+	----------
+	cb : 
+		Obstacle data update callback function
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	global SIMONEAPI_GROUNDTRUTH_CB
 	SimoneAPI.SetGroundTruthUpdateCB(simoneapi_groundtruth_cb_func)
 	SIMONEAPI_GROUNDTRUTH_CB = cb
 
 def SoGetRadarDetections(mainVehicleId, sensorId, detectionData):
+	"""得到毫米波雷达目标信息.
+
+	Get millimeter wave radar detections
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	sensorId : 
+		Sensor Index
+	detectionData : 
+		Radar detections
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetRadarDetections.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	_sensorId = create_string_buffer(sensorId.encode(), 256)
 	return SimoneAPI.GetRadarDetections(_mainVehicleId, _sensorId, pointer(detectionData))
 
 def SoApiSetRadarDetectionsUpdateCB(cb):
+	"""毫米波雷达目标信息回调.
+
+	Register the callback func applying for Millimeter wave radar detections
+
+	Parameters
+	----------
+	cb : 
+		Radar detections update callback function
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	global SIMONEAPI_RADARDETECTION_CB
 	SimoneAPI.SetRadarDetectionsUpdateCB(simoneapi_radardetection_cb_func)
 	SIMONEAPI_RADARDETECTION_CB = cb
 
 def SoGetUltrasonicRadar(mainVehicleId, sensorId, ultrasonics):
+	"""获得一个超声波雷达信息.
+
+	Get UltrasonicRadar imformations
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	sensorId : 
+		Sensor Index
+	ultrasonics : 
+		ultrasonic data in SimOne_Data_UltrasonicRadar format
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetUltrasonicRadar.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	_sensorId = create_string_buffer(sensorId.encode(), 256)
 	return SimoneAPI.GetUltrasonicRadar(_mainVehicleId, _sensorId, pointer(ultrasonics))
 
 def SoGetUltrasonicRadars(mainVehicleId, ultrasonics):
+	"""获得所有超声波雷达信息.
+
+	Get UltrasonicRadars imfomations
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	ultrasonics : 
+		ultrasonics data in SimOne_Data_UltrasonicRadars format
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetUltrasonicRadar.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	return SimoneAPI.GetUltrasonicRadars(_mainVehicleId, pointer(ultrasonics))
 
 def SoApiSetUltrasonicRadarsCB(cb):
+	"""超生波雷达真值信息更新回调.
+
+	Register the callback func applying for ultrasonics radar detections
+
+	Parameters
+	----------
+	cb : 
+		Ultrasonics data update callback function
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	global SIMONEAPI_ULTRASONICS_CB
 	SimoneAPI.SetUltrasonicRadarsCB(simoneapi_ultrasonics_cb_func)
 	SIMONEAPI_ULTRASONICS_CB = cb
 
 def SoGetSensorDetections(mainVehicleId, sensorId, sensorDetections):
+	"""获取传感器检测到物体的对应真值.
+
+	Get Ground Truth objects for current sensor, support camera, lidar and perfect perception sensors
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	sensorId : 
+		Sensor Index
+	sensorDetections : 
+		SimOne_Data_SensorDetections data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	_sensorId = create_string_buffer(sensorId.encode(), 256)
 	SimoneAPI.GetSensorDetections.restype = c_bool
 	return SimoneAPI.GetSensorDetections(_mainVehicleId, _sensorId, pointer(sensorDetections))
 
 def SoApiSetSensorDetectionsUpdateCB(cb):
+	"""传感器真值信息更新回调.
+
+	Register the callback func applying for GroundTruth of current sensor, support camera, lidar and perfect perception sensors
+
+	Parameters
+	----------
+	cb : 
+		Groundtruth data fetch callback function
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	global SIMONEAPI_SENSOR_DETECTIONS_CB
 	SimoneAPI.SetSensorDetectionsUpdateCB(simoneapi_sensor_detections_cb_func)
 	SIMONEAPI_SENSOR_DETECTIONS_CB = cb
 
 def SoGetSensorConfigurations(mainVehicleId, sensorConfigurations):
+	"""得到所有传感器的配置信息（Id、类型、频率、位置和朝向等）.
+
+	Get Sensor's position information
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	sensorConfigurations : 
+		SensorConfigurations data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetSensorConfigurations.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	return SimoneAPI.GetSensorConfigurations(_mainVehicleId, pointer(sensorConfigurations))
 
 def SoGetEnvironment(pEnvironment):
+	"""获取当前环境相关信息（天气、光照、地面等）.
+
+	Get current Environment
+
+	Parameters
+	----------
+	pEnvironment : 
+		SimOne_Data_Environment data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetEnvironment.restype = c_bool
 	return SimoneAPI.GetEnvironment(pointer(pEnvironment))
 
 def SoSetEnvironment(pEnvironment):
+	"""设置当前环境相关信息（天气、光照、地面等）.
+
+	Set Current Environment
+
+	Parameters
+	----------
+	pEnvironment : 
+		SimOne_Data_Environment data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.SetEnvironment.restype = c_bool
 	return SimoneAPI.SetEnvironment(pointer(pEnvironment))
 
 def SoGetTrafficLights(mainVehicleId, opendriveLightId, trafficLight):
+	"""得到仿真场景中的交通灯的真值.
+
+	Get traffic lights Data of Objects(light) from simulation scene
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	opendriveLightId : 
+		traffic light Id on opendrive
+	trafficLight : 
+		light data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetTrafficLight.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	return SimoneAPI.GetTrafficLight(_mainVehicleId, opendriveLightId, pointer(trafficLight))
 
 def SoGetSensorLaneInfo(mainVehicleId, sensorId,pLaneInfo):
+	"""获取传感器检测到车道与车道线数据.
+
+	Get LaneInfo for current sensor, support camera and fusion sensor
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	sensorId : 
+		Sensor Index
+	pLaneInfo : 
+		SimOne_Data_LaneInfo data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetSensorLaneInfo.restype = c_bool
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	_sensorId = create_string_buffer(sensorId.encode(), 256)
 	return SimoneAPI.GetSensorLaneInfo(_mainVehicleId, _sensorId, pointer(pLaneInfo))
 
 def SoApiSetSensorLaneInfoCB(cb):
+	"""获取传感器检测到车道与车道线数据回调.
+
+	Register the callback func applying for LaneInfo from current sensor, support camera, and fusion sensors
+
+	Parameters
+	----------
+	cb : 
+		Groundtruth data fetch callback function
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	global SIMONEAPI_SENSORLANEINFO_CB
 	SimoneAPI.SetSensorLaneInfoCB(simoneapi_sensorlaneinfo_cb_func)
 	SIMONEAPI_SENSORLANEINFO_CB = cb
--- src/python/CommonAPI/SimOneServiceAPI.py
+++ src/python/CommonAPI/SimOneServiceAPI.py
@@ -50,10 +50,41 @@ api_framestart_cb = SimOne_FrameStartFuncType(_api_framestart_cb)
 api_framestop_cb = SimOne_FrameEndFuncType(_api_framestop_cb)
 
 def SoAPIGetVersion():
+	"""获取当前库的版本号.
+
+	Get the version number of the current library
+
+        Parameters
+        ----------
+	None
+
+        Returns
+        -------
+	c_char_p
+            version number
+
+	"""
 	SimoneAPI.GetVersion.restype = c_char_p
 	return SimoneAPI.GetVersion()
 
 def SoSetLogOut(logLevel,*args):
+	"""日志设置接口.
+
+	Set log interface
+
+	Parameters
+	----------
+	logLevel : 
+		warning,error,info flag:true/false
+	args : 
+		output format
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	print(logLevel)
 	list = ""
 	for arg in args:
@@ -62,6 +93,32 @@ def SoSetLogOut(logLevel,*args):
 	return SimoneAPI.SetLogOut(logLevel,logStr)
 
 def SoInitSimOneAPI(mainVehicleId='0',isFrameSync = 0,serverIP = '127.0.0.1',port=23789,startcase = 0, stopcase= 0,registerNodeId=0):
+	"""初始化SimOne API.
+
+	Initialize SimOneAPI for autonomous driving algorithm
+
+	Parameters
+	----------
+	mainVehicleId : 
+		host vehicle ID(from 0 to 9)
+	isFrameSync : 
+		synchronize frame or not
+	serverIP : 
+		BridgeIO server ip
+	port : 
+		BridgeIO server port
+	startcase : 
+		callback func which being called before case start
+	stopcase : 
+		callback func which being called after case end
+	registerNodeId : 
+		not in use
+
+	Returns
+	-------
+	None
+
+	"""
 	_input = create_string_buffer(serverIP.encode(), 256)
 	_mainVehicleId = create_string_buffer(mainVehicleId.encode(), 256)
 	global G_API_StartCase_CB
@@ -76,30 +133,134 @@ def SoInitSimOneAPI(mainVehicleId='0',isFrameSync = 0,serverIP = '127.0.0.1',por
 	return ret
 
 def SoTerminateSimOneAPI():
+	"""退出API node.
+
+	Stop SimOne API node
+
+	Parameters
+	----------
+	None
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.TerminateSimOneAPI.restype = c_bool
 	return SimoneAPI.TerminateSimOneAPI()
 
 def SoAPIGetCaseInfo(data):
+	"""获取案例详情.
+
+	Get case information
+
+	Parameters
+	----------
+	data : 
+		caseName,caseId,taskId,sessionId
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetCaseInfo.restype = c_bool
 	return SimoneAPI.GetCaseInfo(pointer(data))
 
 def SoGetCaseRunStatus():
+	"""获取案例运行情况（运行中，停止）.
+
+	Get case running status
+
+	Parameters
+	----------
+	None
+
+	Returns
+	-------
+	bool
+		Stop,Running
+
+	"""
 	SimoneAPI.GetCaseRunStatus.restype = c_int
 	return SimoneAPI.GetCaseRunStatus()
 
 def SoGetMainVehicleList(data):
+	"""获取主车信息列表，只需要获取一次.
+
+	Get the main vehicle information list
+
+	Parameters
+	----------
+	data : 
+		mainvehicle id/num/type data(output)
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetMainVehicleList.restype = c_bool
 	return SimoneAPI.GetMainVehicleList(pointer(data))
 
 def SoAPIWait():
+	"""获取当前帧值.
+
+	Get the current frame value
+
+	Parameters
+	----------
+	None
+
+	Returns
+	-------
+	bool
+		frame value
+
+	"""
 	SimoneAPI.Wait.restype = c_int
 	return SimoneAPI.Wait()
 
 def SoAPINextFrame(frame):
+	"""进行下一帧.
+
+	Go to the next frame
+
+	Parameters
+	----------
+	frame : 
+		current frame value
+
+	Returns
+	-------
+	bool
+		None
+
+	"""
 	SimoneAPI.NextFrame.restype = c_void_p
 	return SimoneAPI.NextFrame(frame)
 
 def SoAPISetFrameCB(startcb, stopcb):
+	"""仿真场景中每帧的回调,每帧开始和结束时调用回调函数.
+
+	Register the callback func which being called at the beginning and end of each frame
+
+	Parameters
+	----------
+	startcb : 
+		callback func which being called at the beginning of the frame
+	stopcb : 
+		callback func which being called at the end of the frame
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.SetFrameCB.restype = c_bool
 	global G_API_FrameStart_CB
 	global G_API_FrameStop_CB
@@ -113,10 +274,42 @@ def SoAPISetFrameCB(startcb, stopcb):
 	return ret
 
 def SoGetMainVehicleStatus(mainVehicleId, data):
+	"""获取主车状态信息.
+
+	Get the status information of the mainvehicle
+
+	Parameters
+	----------
+	mainVehicleId : 
+		Id of the main vehicle
+	data : 
+		status data which applied for
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetMainVehicleStatus.restype = c_bool
 	return SimoneAPI.GetMainVehicleStatus(mainVehicleId, pointer(data))
 
 def SoAPISetMainVehicleStatusUpdateCB(cb):
+	"""获取主车状态信息回调.
+
+	Register the callback func applying for status info of the mainvehicle
+
+	Parameters
+	----------
+	cb : 
+		callback func applying for status info of the mainvehicle
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.SetMainVehicleStatusUpdateCB.restype = c_bool
 	global G_API_MainVehicleChangeStatusCB
 	if cb == 0:
@@ -125,5 +318,20 @@ def SoAPISetMainVehicleStatusUpdateCB(cb):
 	return SimoneAPI.SetMainVehicleStatusUpdateCB(api_mainvehiclestatusupdate_cb)
 
 def SoGetHDMapData(hdMap):
+	"""获取高精度地图标识.
+
+	Get the hdmap data which is designated by configuring on SimOne web app
+
+	Parameters
+	----------
+	hdMap : 
+		SimOne_Data_Map data
+
+	Returns
+	-------
+	bool
+		Success or not
+
+	"""
 	SimoneAPI.GetHDMapData.restype = c_bool
 	return SimoneAPI.GetHDMapData(pointer(hdMap))
