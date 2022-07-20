7ea0c44136e19677138fc88b5eb1e76f604824fd
gaofeng <gaofeng@51hitech.com>
2022-07-20 11:00:59 +0800

    Merge branch 'cherry-pick-6cc35f76' into 'master'
    update linux python env
    
    See merge request 51World/simoneapi!79
    
    (cherry picked from commit c5f203c0a8a25a1da9a810b39e359c254c11010f)
    
    3e82c53f update linux python env

--- Build/gen_make_debug.sh
+++ Build/gen_make_debug.sh
@@ -10,5 +10,5 @@ rm -rf ../Samples/lib
 rm -rf build_debug
 mkdir build_debug
 cd build_debug
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -DCMAKE_BUILD_NAME=SimOneAPI
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles"  -DPYTHON_INCLUDE_DIR=..\..\..\3rdparty\artifactory\python36  -DPYTHON_LIBRARY=..\..\..\3rdparty\artifactory\python36\libs -DCMAKE_BUILD_NAME=SimOneAPI
 
