6cc35f769eb341312129e19b3e70e24c3d44d61a
gaofeng <gaofeng@51hitech.com>
2022-07-20 10:59:33 +0800

    update linux python env

--- Build/gen_make_debug.sh
+++ Build/gen_make_debug.sh
@@ -8,5 +8,5 @@ cd $SamplePath
 rm -rf build_debug
 mkdir build_debug
 cd build_debug
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles" -DCMAKE_BUILD_NAME=SimOneAPI
+cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -DCMAKE_BUILD_TYPE=Debug -G "Unix Makefiles"  -DPYTHON_INCLUDE_DIR=..\..\..\3rdparty\artifactory\python36  -DPYTHON_LIBRARY=..\..\..\3rdparty\artifactory\python36\libs -DCMAKE_BUILD_NAME=SimOneAPI
 
